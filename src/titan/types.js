b03d751105c66e91e8e71f67abb1c64caddc934c
kuwq8 <kaka345ka@gmail.com>
2026-09-13 04:45:46 +0300

    Add Titan agent role definitions

---
+++ src/titan/types.js
@@ -0,0 +1,9 @@
+const AGENTS = Object.freeze({
+  ARCHITECT: 'architect',
+  LIBRARIAN: 'librarian',
+  BUILDER: 'builder',
+  REVIEWER: 'reviewer',
+  TESTER: 'tester'
+});
+
+module.exports = { AGENTS };
